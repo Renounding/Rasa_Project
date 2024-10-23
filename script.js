4faa59c7b2d86210e6f70bfb610d48d00a9fc0e7
Jay Jackson <jacksonjamiek@gmail.com>
2024-10-23 00:20:45 -0600

    Integration Attempt

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function sendMessage() {
         addMessageToChat('User', message);
 
         // Send message to Rasa server via the ngrok URL
-        fetch("https://d5c4-142-110-39-187.ngrok-free.app/webhooks/rest/webhook", {
+        fetch("https://144f-205-206-111-153.ngrok-free.app/webhooks/rest/webhook", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
